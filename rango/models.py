85f9c4ffdcde7a42da9680c2d9c3fcb7d3075b0f
charles-wood <charles.wood@capco.com>
2014-05-21 23:51:46 +0100

    Adding deleted models back in

--- rango/models.py
+++ rango/models.py
@@ -4,55 +4,55 @@ from django.forms import ModelForm
 from datetime import datetime
 # Create your models here.
 
-# class SiteUser(models.User):
-# 	first_name = models.CharField(max_length=100, null=True, blank=True)
-# 	last_name = models.CharField(max_length=100, null=True, blank=True)
-# 	email = models.EmailField(max_length=254, null=True, blank=False)
-# 	username = models.CharField(max_length=50, null=False, blank=False)
-# 	password = models.CharField(max_length=50, null=False, blank=False)
+class SiteUser(models.User):
+	first_name = models.CharField(max_length=100, null=True, blank=True)
+	last_name = models.CharField(max_length=100, null=True, blank=True)
+	email = models.EmailField(max_length=254, null=True, blank=False)
+	username = models.CharField(max_length=50, null=False, blank=False)
+	password = models.CharField(max_length=50, null=False, blank=False)
 
-# 	def __unicode__(self):
-# 		return str(self.first_name) + ' ' + str(self.last_name) + ', ' + str(self.username)
+	def __unicode__(self):
+		return str(self.first_name) + ' ' + str(self.last_name) + ', ' + str(self.username)
 
-# 	@classmethod
-# 	def create(cls, first_name, last_name, email, username, password):
+	@classmethod
+	def create(cls, first_name, last_name, email, username, password):
 
-# 		success_dict = {
-# 			'error': '',
-# 			'result': '',
-# 			'success': False,
-# 		}
+		success_dict = {
+			'error': '',
+			'result': '',
+			'success': False,
+		}
 
-# 		identical_usernames = SiteUser.objects.filter(username__iexact=username)
-# 		identical_first_last_names = SiteUser.objects.filter(first_name__iexact=first_name).filter(last_name__iexact=last_name)
+		identical_usernames = SiteUser.objects.filter(username__iexact=username)
+		identical_first_last_names = SiteUser.objects.filter(first_name__iexact=first_name).filter(last_name__iexact=last_name)
 
-# 		if len(identical_usernames) > 0:
-# 			success_dict['error'] += 'username already taken'
-# 			success_dict['result'] = None
-# 			success_dict['success'] = False
+		if len(identical_usernames) > 0:
+			success_dict['error'] += 'username already taken'
+			success_dict['result'] = None
+			success_dict['success'] = False
 
-# 		elif len(identical_first_last_names) > 0:
-# 			usernames = [user.username for user in identical_first_last_names]
-# 			success_dict['error'] += "you've already registered, your username is one of " + usernames
-# 			success_dict['result'] = None
-# 			success_dict['success'] = False
+		elif len(identical_first_last_names) > 0:
+			usernames = [user.username for user in identical_first_last_names]
+			success_dict['error'] += "you've already registered, your username is one of " + usernames
+			success_dict['result'] = None
+			success_dict['success'] = False
 
-# 		else:
-# 			user = cls(
-# 				first_name=first_name,
-# 				last_name=last_name,
-# 				email=email,
-# 				username=username,
-# 				password=password
-# 				)
+		else:
+			user = cls(
+				first_name=first_name,
+				last_name=last_name,
+				email=email,
+				username=username,
+				password=password
+				)
 
-# 			user.save()
+			user.save()
 
-# 			success_dict['error'] += ''
-# 			success_dict['result'] = user
-# 			success_dict['success'] = True
+			success_dict['error'] += ''
+			success_dict['result'] = user
+			success_dict['success'] = True
 
-# 		return success_dict
+		return success_dict
 
 class WallPost(models.Model):
 	post = models.CharField(max_length=4000, null=False, blank=False)
